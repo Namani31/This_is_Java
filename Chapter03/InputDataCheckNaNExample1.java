442fc5b7e686276b1c8475899bf53263134ec82b
Namani <kwg531@naver.com>
2023-07-02 02:23:19 +0900

    "NaN" 문자열의 문제점

---
+++ Chapter03/InputDataCheckNaNExample1.java
@@ -0,0 +1,10 @@
+package Chapter03;
+
+public class InputDataCheckNaNExample1 {
+    public static void main(String[] args) {
+        String userInput = "NaN";
+        double val = Double.valueOf(userInput);
+
+        double currentBalance = 10000.0;
+    }
+}
