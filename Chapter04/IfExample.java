5a37792b4cad0c5ca928cea5581882b22c391289
Namani <kwg531@naver.com>
2023-07-05 00:19:11 +0900

    if문

---
+++ Chapter04/IfExample.java
@@ -0,0 +1,16 @@
+package Chapter04;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int score = 93;
+
+        if (score >= 90) {
+            System.out.println("점수가 90보다 큽니다.");
+            System.out.println("등급은 A 입니다.");
+        }
+
+        if (score < 90)
+            System.out.println("점수가 90보다 작습니다.");
+            System.out.print("등급은 B 입니다."); // 중괄호 없으므로 무조건 실행 되는 문장
+    }
+}
