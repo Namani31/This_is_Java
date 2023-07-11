79d6a6e16f30e41f3e62a8de3426db279459ad81
Namani <kwg531@naver.com>
2023-07-11 19:53:01 +0900

    메소드 오버로딩

--- Chapter06/CalculatorExample.java
+++ Chapter06/CalculatorExample.java
@@ -3,6 +3,14 @@ package Chapter06;
 public class CalculatorExample {
     public static void main(String[] args) {
         Calculator myCalc = new Calculator();
-        myCalc.execute();
+
+        // 정사각형의 넓이
+        double result1 = myCalc.areaRectangle(10);
+
+        // 직사각형의 넓이
+        double result2 = myCalc.areaRectangle(10, 20);
+
+        System.out.println("정사각형 넓이=" + result1);
+        System.out.println("직사각형 넓이=" + result2);
     }
 }
