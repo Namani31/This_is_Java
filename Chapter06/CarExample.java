8236c071f294df1e21aaff895f7e0ac1ae084df9
Namani <kwg531@naver.com>
2023-07-11 17:49:10 +0900

    클래스 외부에서 메소드 호출

--- Chapter06/CarExample.java
+++ Chapter06/CarExample.java
@@ -3,19 +3,9 @@ package Chapter06;
 public class CarExample {
     public static void main(String[] args) {
         Car myCar = new Car();
-
-        myCar.setGas(5);    // Car의 setGas() 메소드 호출
-
-        boolean gasState = myCar.isLeftGas();
-        if (gasState) {
-            System.out.println("출발합니다.");
-            myCar.run();
-        }
-
-        if (myCar.isLeftGas()) {
-            System.out.println("gas를 주입할 필요가 없습니다.");
-        } else {
-            System.out.println("gas를 주입하세요.");
-        }
+        myCar.keyTurnOn();
+        myCar.run();
+        int speed = myCar.getSpeed();
+        System.out.println("현재 속도: " + speed + "km/h");
     }
 }
