fa5382b0e1c64b3aa081729ff475f921f30a4b3e
Namani <kwg531@naver.com>
2023-07-20 00:08:33 +0900

    부모 클래스 오타 수정

--- Chapter07/CellPhone.java
+++ Chapter07/CellPhone.java
@@ -15,7 +15,7 @@ public class CellPhone {
         System.out.println("전원을 끕니다.");
     }
     void bell() {
-        System.out.println("벵리 울립니다.");
+        System.out.println("벨이 울립니다.");
     }
     void sendVoice(String message) {
         System.out.println("자기: " + message);
