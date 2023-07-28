f21c6e4280f2ef32b3833a30a9565b91e7cd8ad1
Namani <kwg531@naver.com>
2023-07-28 22:55:09 +0900

    protected 접근 제한자 테스트

---
+++ Chapter07/protectedTest/C.java
@@ -0,0 +1,13 @@
+package Chapter07.protectedTest;
+
+import Chapter07.A;
+
+public class C {
+    public class C {
+        public void method() {
+            A a = new A();  // X
+            a.field = "value";      // X
+            a.method();     // X
+        }
+    }
+}
