2559a4d73cb7f22ef59f14aeafcb4475213bb856
Namani <kwg531@naver.com>
2023-07-31 22:40:42 +0900

    Tire의 자식 클래스

--- Chapter07/HankookTire.java
+++ Chapter07/HankookTire.java
@@ -12,7 +12,7 @@ public class HankookTire extends Tire {
         ++accmulatedRotation;
         if (accmulatedRotation < maxRotation) {
             System.out.println(location + " HankookTire 수명: " + (maxRotation - accmulatedRotation) + "회");
-            return false;
+            return true;
         } else {
             System.out.println("*** " + location + " HankookTire 펑크 ***");
             return false;
