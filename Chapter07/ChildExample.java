0b4b3e1ba16ae735b791dcdae1e95bed97401a21
Namani <kwg531@naver.com>
2023-08-02 03:43:41 +0900

    강제 타입 변환 (캐스팅)

--- Chapter07/ChildExample.java
+++ Chapter07/ChildExample.java
@@ -2,11 +2,17 @@ package Chapter07;
 
 public class ChildExample {
     public static void main(String[] args) {
-        Child child = new Child();
-
-        Parent parent = child;  // 자동 타입 변환
+        Parent parent = new Child();    // 자동 타입 변환
+        parent.field = "data1";
         parent.mathod1();
-        parent.method2();   // 재정의 된 메소드가 호출 됨
-        // parent.method3(); // 호출 불가
+        parent.method2();
+        /*
+            parent.field2 = "data2"; // 불가능
+            parent.method3(); // 불가능
+         */
+
+        Child child = (Child) parent;   // 강제 타입 변환
+        child.field2 = "yyy";
+        child.method3();
     }
 }
