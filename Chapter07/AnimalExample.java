f3480a549a9deb56bea12ca8deedafd91601e9db
Namani <kwg531@naver.com>
2023-08-09 02:15:08 +0900

    실행 클래스

---
+++ Chapter07/AnimalExample.java
@@ -0,0 +1,29 @@
+package Chapter07;
+
+public class AnimalExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        dog.sound();
+        cat.sound();
+        System.out.println("-----");
+
+        // 변수의 자동 타입 변환
+        Animal animal = null;
+        animal = new Dog(); // 자동 타입 변환
+        animal.sound(); // 재정의 된 메소드 호출
+        animal = new Cat();
+        animal.sound();
+        System.out.println("-----");
+
+        // 메소드의 다형성
+        animalSound(new Dog());
+        animalSound(new Cat());
+    }
+
+    public static void animalSound(Animal animal) {
+        // Cat()이 Animal로 들어감, 자동 타입 변환
+        // Dog()가 animal로 들어감, 자동 타입 변환
+        animal.sound(); // 재정의 된 메소드 호출
+    }
+}
