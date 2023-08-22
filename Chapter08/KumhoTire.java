78e0d4cb0423abe60e3519a479d03a754b26fe80
Namani <kwg531@naver.com>
2023-08-22 23:28:36 +0900

    구현 클래스 (Kumho Tire)

---
+++ Chapter08/KumhoTire.java
@@ -0,0 +1,9 @@
+package Chapter08;
+
+public class KumhoTire implements Tire {
+    // roll 인터페이스 구현
+    @Override
+    public void roll() {
+        System.out.println("금호 타이어가 굴러갑니다.");
+    }
+}
