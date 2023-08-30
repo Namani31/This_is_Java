aa795b5cc46c112e56baff2d9bcbf3206b057d34
Namani <kwg531@naver.com>
2023-08-30 21:50:02 +0900

    하위 인터페이스 구현

--- Chapter08/ImplementationC.java
+++ Chapter08/ImplementationC.java
@@ -9,11 +9,11 @@ public class ImplementationC implements InterfaceC {
 
     @Override
     public void methodB() {
-        System.out.println("ImplementationC-methodA() 실행");
+        System.out.println("ImplementationC-methodB() 실행");
     }
 
     @Override
     public void methodC() {
-
+        System.out.println("ImplementationC-methodC() 실행");
     }
 }
