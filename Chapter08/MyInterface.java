df9df1860655dbd63090c4751ae84a8cfaf50784
Namani <kwg531@naver.com>
2023-09-02 01:01:06 +0900

    수정 인터페이스

--- Chapter08/MyInterface.java
+++ Chapter08/MyInterface.java
@@ -2,4 +2,8 @@ package Chapter08;
 
 public interface MyInterface {
     public void method1();
+
+    public default void method2() {
+        System.out.println("MyInterface-method2 실행");
+    }
 }
