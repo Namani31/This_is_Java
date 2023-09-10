1b2b129363545e3d4a6dc68fb184ccee8c7c38dc
Namani <kwg531@naver.com>
2023-09-11 00:23:02 +0900

    멤버 클래스에서 사용 제한

--- Chapter09/A.java
+++ Chapter09/A.java
@@ -1,31 +1,33 @@
 package Chapter09;
 
-/* 바깥 클래스 */
 class A {
-    // 인스턴스 필드
-    B field1 = new B(); // (o)
-    C field2 = new C(); // (o)
+    int field1;
 
-    // 인스턴스 메소드
     void method1() {
-        B var1 = new B();   // (o)
-        C var2 = new C();   // (o)
     }
 
-    // 정적 필드 초기화
-    // static B field3 = new B(); (x)
-    static C field4 = new C();  // (o)
+    static int field2;
 
-    // 정적 메소드
     static void method2() {
-        // B var1 = new B();    // (x)
-        C var2 = new C();   // (o)
     }
 
-    // 인스턴스 멤버 클래스
-    class B {}
+    class B {
+        void method() {
+            void field1 = 10;
+            method1();
 
-    // 정적 멤버 클래스
-    static class C{}
+            field2 = 10;
+            method2();
+        }
+    }
 
+    static class C {
+        void method() {
+            // field1 = 10;
+            // method1();
+
+            field2 = 10;
+            method2();
+        }
+    }
 }
