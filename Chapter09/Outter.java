d4815c29124edab76b82267387f185b3ff558ccd
Namani <kwg531@naver.com>
2023-09-12 00:06:49 +0900

    중첩 클래스에서 바깥 클래스 참조 얻기

--- Chapter09/Outter.java
+++ Chapter09/Outter.java
@@ -1,27 +1,22 @@
 package Chapter09;
 
 public class Outter {
-    // 자바 7 이전
-    public void method1(final int arg) {
-        final int localVariable = 1;
-        // arg = 100; (x)
-        // localVariable = 100; (x)
-        class Inner {
-            public void method() {
-                int result = arg + localVariable;
-            }
-        }
+    String field = "Outter-field";
+
+    void method() {
+        System.out.println("Outter-method");
     }
 
-    // 자바 8 이후
-    public void method2(int arg) {
-        int localVariable = 1;
-        // arg = 100; (x)
-        // localVariable = 100; (x)
-        class Inner {
-            public void method() {
-                int result = arg + localVariable;
-            }
+    class Nested {
+        String field = "Nested-field";
+        void method() {
+            System.out.println("Nested-field");
+        }
+        void print() {
+            System.out.println(this.field);
+            this.method();
+            System.out.println(Outter.this.field);
+            Outter.this.method();
         }
     }
 }
