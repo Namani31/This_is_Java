de412c4ebf7c02676b149193ff5227eccc9268ca
Namani <kwg531@naver.com>
2023-09-19 00:09:26 +0900

    익명 구현 클래스와 객체 생성

--- Chapter09/Annoymous.java
+++ Chapter09/Annoymous.java
@@ -2,33 +2,36 @@ package Chapter09;
 
 public class Annoymous {
     // 필드 초기값으로 대입
-    Person field = new Person() {
+    RemoteControl field = new RemoteControl() {
         @Override
-        void wake() {
-            System.out.println("출근합니다.");
+        public void turnOn() {
+            System.out.println("TV를 켭니다.");
         }
-        void work() {
-            System.out.println("6시에 일어납니다.");
-            work();
+
+        @Override
+        public void turnOff() {
+            System.out.println("TV를 끕니다.");
         }
     };
 
     void method1() {
-        Person localVar = new Person() {
-            void walk() {
-                System.out.println("산책합니다.");
-            }
+        // 로컬 변수 값으로 대입
+        RemoteControl localVar = new RemoteControl() {
             @Override
-            void wake() {
-                System.out.println("7시에 일어납니다.");
-                walk();
+            public void turnOn() {
+                System.out.println("Audio를 켭니다.");
+            }
+
+            @Override
+            public void turnOff() {
+                System.out.println("Audio를 끕니다.");
             }
         };
         // 로컬 변수 사용
-        localVar.wake();
+        localVar.turnOn();
     }
 
-    void method2(Person person) {
-        person.wake();
+    void method2(RemoteControl rc) {
+        rc.turnOn();
     }
 }
