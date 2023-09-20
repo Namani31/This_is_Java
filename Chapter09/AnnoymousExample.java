f140a8a79fd5689610965cd8dbd17daec4b714e4
Namani <kwg531@naver.com>
2023-09-21 01:11:45 +0900

    익명 구현 클래스와 객체 생성

--- Chapter09/AnnoymousExample.java
+++ Chapter09/AnnoymousExample.java
@@ -5,22 +5,22 @@ public class AnnoymousExample {
         Annoymous annoy = new Annoymous();
 
         // 익명 객체 필드 사용
-        annoy.field.wake();
+        annoy.field.turnOn();
 
         // 익명 객체 로컬 변수 사용
         annoy.method1();
 
         // 익명 객체 매개값 사용
         annoy.method2(
-                new Person() {
-                    void study() {
-                        System.out.println("공부합니다.");
+                new RemoteControl() {
+                    @Override
+                    public void turnOn() {
+                        System.out.println("SmartTV를 켭니다.");
                     }
 
                     @Override
-                    void wake() {
-                        System.out.println("8시에 일어납니다.");
-                        study();
+                    public void turnOff() {
+                        System.out.println("SmartTV를 끕니다.");
                     }
                 }
         );
