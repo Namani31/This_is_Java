7f6f76fcd8bead34828d17c5abbb74f3f5835d1c
Namani <kwg531@naver.com>
2023-09-27 19:54:21 +0900

    실행 메서드

--- Chapter09/AnnoymousExample.java
+++ Chapter09/AnnoymousExample.java
@@ -3,26 +3,6 @@ package Chapter09;
 public class AnnoymousExample {
     public static void main(String[] args) {
         Annoymous annoy = new Annoymous();
-
-        // 익명 객체 필드 사용
-        annoy.field.turnOn();
-
-        // 익명 객체 로컬 변수 사용
-        annoy.method1();
-
-        // 익명 객체 매개값 사용
-        annoy.method2(
-                new RemoteControl() {
-                    @Override
-                    public void turnOn() {
-                        System.out.println("SmartTV를 켭니다.");
-                    }
-
-                    @Override
-                    public void turnOff() {
-                        System.out.println("SmartTV를 끕니다.");
-                    }
-                }
-        );
+        annoy.method(0, 0);
     }
 }
