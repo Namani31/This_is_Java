6ac428db331cd8eb1546439779d1d1dfeec2b56a
Namani31 <kwg531@naver.com>
2023-10-07 11:54:34 +0900

    ClassCastException

---
+++ Chapter10/ClassCastExceptionExample.java
@@ -0,0 +1,21 @@
+package Chapter10;
+
+public class ClassCastExceptionExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        changeDog(dog);
+
+        Cat cat = new Cat();
+        changeDog(cat);
+    }
+
+    public static void changeDog(Animal animal) {
+        // if (animal instaceof Dog) {
+            Dog dog = (Dog) animal; // ClassCastException 발생 가능
+        // }
+    }
+}
+
+class Animal {}
+class Dog extends Animal {}
+class Cat extends Animal {}
