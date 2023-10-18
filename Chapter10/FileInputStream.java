ce39f972171a2b0349d290957c911410d6b7d844
Namani <kwg531@naver.com>
2023-10-18 14:29:48 +0900

    AutoCloseable 구현 클래스

---
+++ Chapter10/FileInputStream.java
@@ -0,0 +1,13 @@
+package Chapter10;
+
+public class FileInputStream implements AutoCloseable {
+    private String file;
+
+    public FileInputStream(String file) {
+        this.file = file;
+    }
+
+    public void read() {
+        System.out.println(file + "을 읽습니다.");
+    }
+}
