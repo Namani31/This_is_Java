6994b7cdf6addbf59061486c4bc3ed17b9b9f129
Namani <kwg531@naver.com>
2023-10-19 23:48:19 +0900

    AutoCloseable 구현 클래스 - 오버라이드 추가

--- Chapter10/FileInputStream.java
+++ Chapter10/FileInputStream.java
@@ -10,4 +10,9 @@ public class FileInputStream implements AutoCloseable {
     public void read() {
         System.out.println(file + "을 읽습니다.");
     }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println(file + "을 닫습니다.");
+    }
 }
