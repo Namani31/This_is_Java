db1e820a28060d8354280afcff90a93f00581bb3
Namani <kwg531@naver.com>
2023-11-14 20:38:05 +0900

    복제할 수 있는 클래스 선언

--- Chapter10/Member.java
+++ Chapter10/Member.java
@@ -1,25 +1,27 @@
 package Chapter10;
 
-public class Member {
+public class Member implements Cloneable {
     public String id;
+    public String name;
+    public String password;
+    public  int age;
+    public boolean adult;
 
-    public Member(String id) {
+    public Member(String id, String name, String password, int age, boolean adult) {
         this.id = id;
+        this.name = name;
+        this.password = password;
+        this.age = age;
+        this.adult = adult;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Member) {
-            Member member = (Member) obj;
-            if (id.equals(member.id)) {
-                return true;
-            }
+    public Member getMember() {
+        Member cloned = null;
+
+        try {// clone() 메소드의 리턴 타입은 Object 이므로 Member 타입으로 캐스팅 해야 함
+            cloned = (Member) clone();
+        } catch (CloneNotSupportedException e) {
+            return cloned;
         }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
     }
 }
