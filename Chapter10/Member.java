ae95c0dca7bdb520c938580bf197ef8215bf05c0
Namani <kwg531@naver.com>
2023-11-16 11:26:18 +0900

    clone()을 재정의해서 깊은 복제로 변경

--- Chapter10/Member.java
+++ Chapter10/Member.java
@@ -1,11 +1,14 @@
 package Chapter10;
 
+import Chapter06.Car;
+
+import java.util.Arrays;
+
 public class Member implements Cloneable {
     public String id;
     public String name;
-    public String password;
-    public int age;
-    public boolean adult;
+    public int[] scores;
+    public Car car;
 
     public Member(String id, String name, String password, int age, boolean adult) {
         this.id = id;
@@ -13,6 +16,18 @@ public class Member implements Cloneable {
         this.password = password;
         this.age = age;
         this.adult = adult;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // 먼저 얕은 복사를 해서 name, age를 복제한다.
+        Member cloned = (Member) super.clone();
+        // scores를 깊은 복제한다.
+        cloned.scores = Arrays.copyOf(this.scores, this.scores.length);
+        // car를 깊은 복제한다.
+        cloned.car = new Car(this.car.model);
+        // 깊은 복제 된 Member 객체를 리턴
+        return cloned;
     }
 
     public Member getMember() {
